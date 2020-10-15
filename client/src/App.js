2c00bc87e6911907a4250fd52c6b9af3418d85a4
MugishaU <muwiragiye123@gmail.com>
2020-10-15 21:45:04 +0100

    add login button to generate test jwt's

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,56 @@
 import React from "react";
+import * as firebase from "firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 import "./styles/App.css";
-class App extends React.Component {
-  state = {};
-  render() {
+
+export default function App() {
+  const [user] = useAuthState(firebase.auth());
+
+  if (user) {
     return (
       <>
-        <h1>New React App</h1>
-        <h2>Happy Coding</h2>
+        <h1>You're Logged In 🎉</h1>
+        <h3>Email: {firebase.auth().currentUser.email}</h3>
+        <button
+          onClick={() => {
+            firebase.app().auth().signOut();
+          }}
+        >
+          Logout
+        </button>
+        <button
+          onClick={() => {
+            firebase
+              .auth()
+              .currentUser.getIdToken(/* forceRefresh */ true)
+              .then(function (idToken) {
+                console.log(idToken);
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+          }}
+        >
+          Get Token
+        </button>
       </>
     );
   }
+  return (
+    <>
+      <h1>Sign In</h1>
+      <button
+        onClick={() => {
+          firebase
+            .auth()
+            .signInWithEmailAndPassword("user1@user.com", "test123")
+            .catch(function (error) {
+              alert(error.message);
+            });
+        }}
+      >
+        Login
+      </button>
+    </>
+  );
 }
-export default App;
