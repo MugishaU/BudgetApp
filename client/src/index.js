3be7bb6887edbd048df0d48365b95c2c0fb6a773
MugishaU <muwiragiye123@gmail.com>
2020-10-15 21:10:50 +0100

    add firebase to client

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import * as firebase from "firebase";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCe2GMRcKbCInRUE54SH1UiciA6U6xAb6c",
+  authDomain: "budgetapp-1110d.firebaseapp.com",
+  databaseURL: "https://budgetapp-1110d.firebaseio.com",
+  projectId: "budgetapp-1110d",
+  storageBucket: "budgetapp-1110d.appspot.com",
+  messagingSenderId: "319333016623",
+  appId: "1:319333016623:web:e9683f17506daa5c5ed2da",
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
