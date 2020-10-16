95d1ec26a25104b0617f79dc8f4dd37951fc46a0
MugishaU <muwiragiye123@gmail.com>
2020-10-16 13:50:18 +0100

    make function checking token

--- server/helpers.py
+++ server/helpers.py
@@ -1,5 +1,26 @@
-from functools import wraps
+# from functools import wraps
+import firebase_admin
+from firebase_admin import auth, credentials
+from os import environ
+from flask import request
+from dotenv import load_dotenv
+load_dotenv()
 
+GOOGLE_APPLICATION_CREDENTIALS = {
+    "type": "service_account",
+    "project_id": environ["FIREBASE_PROJECT_ID"],
+    "private_key_id": environ["FIREBASE_PRIVATE_KEY_ID"],
+    "client_email": environ["FIREBASE_CLIENT_EMAIL"],
+    "client_id": environ["FIREBASE_CLIENT_ID"],
+    "auth_uri": environ["FIREBASE_AUTH_URI"],
+    "token_uri": environ["FIREBASE_TOKEN_URI"],
+    "auth_provider_x509_cert_url": environ["AUTH_PROVIDER_X509_CERT_URL"],
+    "client_x509_cert_url": environ["CLIENT_X509_CERT_URL"],
+    "private_key": environ["FIREBASE_PRIVATE_KEY"].replace("\\n", "\n").replace("\\","")
+}
+
+cred = credentials.Certificate(GOOGLE_APPLICATION_CREDENTIALS)
+firebase_admin.initialize_app(cred)
 
 def format_resp(resultproxy):
     d, a = {}, []
@@ -8,3 +29,11 @@ def format_resp(resultproxy):
             d = {**d, **{column: value}}
         a.append(d)
     return a
+
+def check_token(token):
+    try:
+        decoded_token = auth.verify_id_token(token)
+        uid = decoded_token['uid']
+    except (ValueError,firebase_admin.exceptions.FirebaseError):
+        return ({'error':True,'message':'Invalid Token'})
+    return ({'error':False, 'uid':uid})
--- server/server.py
+++ server/server.py
@@ -6,7 +6,7 @@ from flask import Flask, jsonify, request, session
 from flask_cors import CORS
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
-from helpers import format_resp
+from helpers import check_token, format_resp
 
 load_dotenv()
 
@@ -15,21 +15,7 @@ app = Flask(__name__)
 CORS(app)
 Session(app)
 
-GOOGLE_APPLICATION_CREDENTIALS = {
-    "type": "service_account",
-    "project_id": environ["FIREBASE_PROJECT_ID"],
-    "private_key_id": environ["FIREBASE_PRIVATE_KEY_ID"],
-    "client_email": environ["FIREBASE_CLIENT_EMAIL"],
-    "client_id": environ["FIREBASE_CLIENT_ID"],
-    "auth_uri": environ["FIREBASE_AUTH_URI"],
-    "token_uri": environ["FIREBASE_TOKEN_URI"],
-    "auth_provider_x509_cert_url": environ["AUTH_PROVIDER_X509_CERT_URL"],
-    "client_x509_cert_url": environ["CLIENT_X509_CERT_URL"],
-    "private_key": environ["FIREBASE_PRIVATE_KEY"].replace("\\n", "\n").replace("\\","")
-}
 
-cred = credentials.Certificate(GOOGLE_APPLICATION_CREDENTIALS)
-firebase_admin.initialize_app(cred)
 
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -42,7 +28,10 @@ def running():
 
 @app.route('/register')
 def register():
-    return jsonify("register")
+    auth = check_token("eyJhbGciOiJSUzI1NiIsImtpZCI6IjBlM2FlZWUyYjVjMDhjMGMyODFhNGZmN2RjMmRmOGIyMzgyOGQ1YzYiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vYnVkZ2V0YXBwLTExMTBkIiwiYXVkIjoiYnVkZ2V0YXBwLTExMTBkIiwiYXV0aF90aW1lIjoxNjAyODUxNjYyLCJ1c2VyX2lkIjoieG1hT0RVaUFwUlJvSk1pRkJxWDd2SFNkaHlTMiIsInN1YiI6InhtYU9EVWlBcFJSb0pNaUZCcVg3dkhTZGh5UzIiLCJpYXQiOjE2MDI4NTE2NjQsImV4cCI6MTYwMjg1NTI2NCwiZW1haWwiOiJ1c2VyMUB1c2VyLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJ1c2VyMUB1c2VyLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6InBhc3N3b3JkIn19.exC2auOW9JY8rGafo7acF_WgLX9seQhvu4uueQHehPhHm0EsrxWdIlk78pmHB0LWaj2E4fgL_V5Wen4UkrZATJnE6vMGWj5Dyw_YAW_JLS6BwOO8_gV6RJKWtGc_8Ak6HLNdTdedkHAEaH81do4M9dUl8c-9WgFhORD_TGlUdIjIcPFYeT7F3i9Nhlp2USXZLwZLjDMFXUuqf154rtq-zgDWcL-KbQ_HpxPfT9nsA-Xuwc_mDyVudWpIIPwEx7nXxz1XqY30My2NcPdqyRW0sSS7sBWGaZ8uEzftKu9NgqpRBUQFWsxi-H1w5XdZCT_A7JLBOH7cW8jDM2QJ3EFs3Q")
+    if auth['error'] == True:
+        return jsonify(auth['message'])
+    return jsonify(auth['uid'])
 
 
 app.run(debug=True)
