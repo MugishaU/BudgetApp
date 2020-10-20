ec86ce8bb181b37db15f3a01f7c90b41d6269c33
MugishaU <muwiragiye123@gmail.com>
2020-10-20 13:12:02 +0100

    add react routing on unauthhomepage

--- client/src/Containers/UnauthHomePage.js
+++ client/src/Containers/UnauthHomePage.js
@@ -1,11 +1,20 @@
 import React from "react";
-import { UnauthNavbar, LoginForm } from "../Components/index/index";
+import { Switch, Route } from "react-router-dom";
+import {
+  UnauthNavbar,
+  LoginForm,
+  RegisterForm,
+} from "../Components/index/index";
 
 export default function UnauthHomePage() {
   return (
     <>
       <UnauthNavbar />
-      <LoginForm />
+
+      <Switch>
+        <Route exact path="/" component={LoginForm} />
+        <Route path="/register" component={RegisterForm} />
+      </Switch>
     </>
   );
 }
