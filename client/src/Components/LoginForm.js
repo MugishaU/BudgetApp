f29ecd564113705a117145b8ad094c1be1f9ed60
MugishaU <muwiragiye123@gmail.com>
2020-10-20 13:08:42 +0100

    add title to login form

--- client/src/Components/LoginForm.js
+++ client/src/Components/LoginForm.js
@@ -12,34 +12,37 @@ export default function LoginForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-        login(email, password);
-        setPassword("");
-      }}
-    >
-      <input
-        required
-        type="email"
-        name="email"
-        placeholder="E-mail"
-        value={email}
-        onChange={(event) => {
-          setEmail(event.target.value);
+    <>
+      <h1>Login</h1>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          login(email, password);
+          setPassword("");
         }}
-      />
-      <input
-        required
-        type="password"
-        name="password"
-        placeholder="Password"
-        value={password}
-        onChange={(event) => {
-          setPassword(event.target.value);
-        }}
-      />
-      <input type="submit" name="submit" value="Login" />
-    </form>
+      >
+        <input
+          required
+          type="email"
+          name="email"
+          placeholder="E-mail"
+          value={email}
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
+        />
+        <input
+          required
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={password}
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+        />
+        <input type="submit" name="submit" value="Login" />
+      </form>
+    </>
   );
 }
