cda51dc3713ed7e55104a46047fcab84393297b1
MugishaU <muwiragiye123@gmail.com>
2020-10-20 12:45:18 +0100

    add navbar and login to unauth homepage

--- client/src/Containers/UnauthHomePage.js
+++ client/src/Containers/UnauthHomePage.js
@@ -1,5 +1,11 @@
 import React from "react";
+import { UnauthNavbar, LoginForm } from "../Components/index/index";
 
 export default function UnauthHomePage() {
-  return <div></div>;
+  return (
+    <>
+      <UnauthNavbar />
+      <LoginForm />
+    </>
+  );
 }
