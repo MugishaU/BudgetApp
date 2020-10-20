5088e05075f249f707de3a5cba9476acd8c8ba7c
MugishaU <muwiragiye123@gmail.com>
2020-10-20 13:09:16 +0100

    correct unauth nav routes

--- client/src/Components/UnauthNavbar.js
+++ client/src/Components/UnauthNavbar.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function UnauthNavbar() {
   return (
     <nav>
-      <Link to="/login">Login</Link>
+      <Link to="/">Login</Link>
       <Link to="/register">Register</Link>
     </nav>
   );
