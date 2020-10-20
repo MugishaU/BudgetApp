fa4a2e5b2c7e7c4eed776b5f870556ef8a0c48f2
MugishaU <muwiragiye123@gmail.com>
2020-10-20 18:19:24 +0100

    remove unnecessary functions in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,26 +3,6 @@ import * as firebase from "firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { UnauthHomePage, AuthHomePage } from "./Containers/index/index";
 import "./styles/App.css";
-import { AuthNavbar } from "./Components/index";
-
-const login = () =>
-  firebase
-    .auth()
-    .signInWithEmailAndPassword("user1@user.com", "test123")
-    .catch(function (error) {
-      alert(error.message);
-    });
-const logout = () => firebase.app().auth().signOut();
-const token = () =>
-  firebase
-    .auth()
-    .currentUser.getIdToken(/* forceRefresh */ true)
-    .then(function (idToken) {
-      console.log(idToken);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
 
 export default function App() {
   const [user] = useAuthState(firebase.auth());
