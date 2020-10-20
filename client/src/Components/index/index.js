c5f6f6560c248b37b86cb42ea3e6c63ba197862c
MugishaU <muwiragiye123@gmail.com>
2020-10-20 17:11:02 +0100

    export AuthNavbar

--- client/src/Components/index/index.js
+++ client/src/Components/index/index.js
@@ -1,3 +1,5 @@
 export { default as UnauthNavbar } from "../Unauth/UnauthNavbar";
 export { default as LoginForm } from "../Unauth/LoginForm";
 export { default as RegisterForm } from "../Unauth/RegisterForm";
+
+export { default as AuthNavbar } from "../Auth/AuthNavbar";
