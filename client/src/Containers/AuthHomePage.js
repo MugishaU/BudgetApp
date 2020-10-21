a58685af1e6981e1fac514f6cf72de6d0647e90b
MugishaU <muwiragiye123@gmail.com>
2020-10-21 15:16:16 +0100

    add token to global state and entire portfolio, which calls on login

--- client/src/Containers/AuthHomePage.js
+++ client/src/Containers/AuthHomePage.js
@@ -3,55 +3,50 @@ import * as firebase from "firebase";
 import { AuthNavbar } from "../Components/index/index";
 import { UserContext } from "../Context/userContext ";
 
-const token = () =>
-  firebase
-    .auth()
-    .currentUser.getIdToken(/* forceRefresh */ true)
-    .then(function (idToken) {
-      console.log(idToken);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-
-const token2 = () => {
+const getToken = () => {
   console.log(firebase.auth().currentUser.getIdToken(true));
 };
 
 export default function AuthHomePage() {
-  const { user, setUser } = useContext(UserContext);
+  const { profile, setProfile, setHistory, token } = useContext(UserContext);
 
   useEffect(() => {
-    firebase
-      .auth()
-      .currentUser.getIdToken(/* forceRefresh */ true)
-      .then(function (idToken) {
-        const options = {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            token: idToken,
-          },
-        };
+    let today = new Date();
+    const options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        token: token,
+      },
+    };
+    if (token) {
+      fetch(`https://budgt-app.herokuapp.com/user`, options)
+        .then((result) => result.json())
+        .then((data) => {
+          setProfile(data);
+        })
+        .catch((error) => console.log(error));
 
-        fetch(`https://budgt-app.herokuapp.com/user`, options)
-          .then((result) => result.json())
-          .then((data) => {
-            setUser(data);
-          })
-          .catch((error) => console.log(error));
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }, []);
+      fetch(
+        `https://budgt-app.herokuapp.com/history?month=${
+          today.getMonth() + 1
+        }&year=${today.getFullYear()}`,
+        options
+      )
+        .then((result) => result.json())
+        .then((data) => {
+          setHistory(data);
+        })
+        .catch((error) => console.log(error));
+    }
+  }, [token]);
   return (
     <>
       <AuthNavbar />
       <h1>You're Logged In 🎉</h1>
-      {user && <h1>{user.username}</h1>}
+      {profile && <h1>{profile.username}</h1>}
       <h3>Email: {firebase.auth().currentUser.email}</h3>
-      <button onClick={token2}>Get Token</button>
+      <button onClick={getToken}>Get Token</button>
     </>
   );
 }
--- client/src/Context/userContext .js
+++ client/src/Context/userContext .js
@@ -1,13 +1,36 @@
 import React, { useState, useEffect } from "react";
+import * as firebase from "firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 export const UserContext = React.createContext();
 
 const UserContextProvider = (props) => {
-  const [user, setUser] = useState(null);
-  // const data = "Hello";
+  const [user] = useAuthState(firebase.auth());
+  const [profile, setProfile] = useState(null);
+  const [history, setHistory] = useState([]);
+  const [token, setToken] = useState(null);
+  useEffect(() => {
+    if (firebase.auth().currentUser !== null) {
+      firebase
+        .auth()
+        .currentUser.getIdToken(true)
+        .then(function (idToken) {
+          setToken(idToken);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    } else {
+      setToken(null);
+      setProfile(null);
+      setHistory([]);
+    }
+  }, [user]);
 
   return (
-    <UserContext.Provider value={{ user, setUser }}>
+    <UserContext.Provider
+      value={{ profile, setProfile, history, setHistory, token }}
+    >
       {props.children}
     </UserContext.Provider>
   );
