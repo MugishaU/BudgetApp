53b3e9241141a0c22ce03b58b5256c38651e8c09
MugishaU <muwiragiye123@gmail.com>
2020-10-22 18:11:20 +0100

    finish profile card

--- client/src/Components/Auth/ProfileCard.js
+++ client/src/Components/Auth/ProfileCard.js
@@ -1,5 +1,44 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { UserContext } from "../../Context/userContext ";
 
 export default function ProfileCard() {
-  return <div></div>;
+  const { profile, history } = useContext(UserContext);
+  const [total, setTotal] = useState(0);
+  useEffect(() => {
+    if (history.length > 0) {
+      let total = history.reduce((a, b) => {
+        return a + b.cost;
+      }, 0);
+
+      setTotal(total);
+    }
+  }, [history]);
+  if (profile && history) {
+    return (
+      <div>
+        <h3>Overview</h3>
+        <table>
+          <tbody>
+            <tr>
+              <th>Limit</th>
+              <td>£{profile.budget.toFixed(2)}</td>
+            </tr>
+            <tr>
+              <th>Spent</th>
+              <td>£{total.toFixed(2)}</td>
+            </tr>
+            <tr>
+              <th>Remaining</th>
+              <td>
+                {profile.budget > total
+                  ? `£${(profile.budget - total).toFixed(2)}`
+                  : `-£${-(profile.budget - total).toFixed(2)}`}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+  return null;
 }
