1725525868dd32e675d7eeb56a3a24ea53cc8823
MugishaU <muwiragiye123@gmail.com>
2020-10-22 20:31:35 +0100

    make line graph functionality work

--- client/src/Components/Auth/LineChart.js
+++ client/src/Components/Auth/LineChart.js
@@ -1,5 +1,21 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { UserContext } from "../../Context/userContext ";
+import { Line } from "react-chartjs-2";
 
 export default function LineChart() {
-  return <div></div>;
+  const { history } = useContext(UserContext);
+  //   useEffect();
+  return (
+    <div>
+      <Line
+        data={{
+          labels: ["1", "2", "3", "4"],
+          datasets: [
+            { label: "Spending Test", data: [12, 23, 34, 50] },
+            { label: "Budget Test", data: [20, 20, 20, 20] },
+          ],
+        }}
+      />
+    </div>
+  );
 }
