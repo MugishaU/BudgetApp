676dff953f9d97e0657c395fb3fb0af001af03c1
MugishaU <muwiragiye123@gmail.com>
2020-10-23 16:41:52 +0100

    load spending line graph on login

--- client/src/Components/Auth/LineChart.js
+++ client/src/Components/Auth/LineChart.js
@@ -4,22 +4,63 @@ import { Line } from "react-chartjs-2";
 
 export default function LineChart() {
   const { history } = useContext(UserContext);
+  const [chartData, setChartData] = useState(null);
   useEffect(() => {
     if (history) {
-      let sortedHistory = history.sort((a, b) => a.day - b.day);
+      const sortedHistory = history.sort((a, b) => a.day - b.day);
+      const distinctHistory = [];
+      let position = 0;
+
+      for (const item of sortedHistory) {
+        if (position === 0) {
+          distinctHistory.push(item);
+          position++;
+        } else if (distinctHistory[position - 1].day === item.day) {
+          distinctHistory[position - 1].cost += item.cost;
+        } else {
+          distinctHistory.push(item);
+          position++;
+        }
+      }
+
+      let lastDay = distinctHistory.slice(-1)[0].day;
+      let fullHistory = [];
+      let budget = [];
+      let dayLabel = [];
+      let sortedHistoryPosition = 0;
+      let accumulatedCost = 0;
+      let budgetCost = history[0].budget;
+
+      for (let day = 1; day <= lastDay; day++) {
+        const checkDay = distinctHistory[sortedHistoryPosition];
+        if (day < checkDay.day) {
+          // Do Nothing
+        } else if (day == checkDay.day) {
+          accumulatedCost += checkDay.cost;
+          sortedHistoryPosition++;
+        }
+        fullHistory.push(accumulatedCost);
+        budget.push(budgetCost);
+        dayLabel.push(day);
+      }
+      setChartData({ fullHistory, budget, dayLabel });
     }
   }, [history]);
-  return (
-    <div>
-      <Line
-        data={{
-          labels: ["1", "2", "3", "4"],
-          datasets: [
-            { label: "Spending Test", data: [12, 23, 34, 50] },
-            { label: "Budget Test", data: [20, 20, 20, 20] },
-          ],
-        }}
-      />
-    </div>
-  );
+  if (chartData) {
+    return (
+      <div>
+        <Line
+          data={{
+            labels: chartData.dayLabel,
+            datasets: [
+              { label: "Spending", data: chartData.fullHistory },
+              { label: "Limit", data: chartData.budget },
+            ],
+          }}
+        />
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
