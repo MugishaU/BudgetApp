f178e6bf4ff3306b7a4c1fc224e6a45ce89ed501
MugishaU <muwiragiye123@gmail.com>
2020-10-25 21:52:30 +0000

    form submit complete

--- client/src/Components/Auth/AddSpend.js
+++ client/src/Components/Auth/AddSpend.js
@@ -10,10 +10,10 @@ export default function AddSpend() {
   const [date, setDate] = useState(today.toISOString().slice(0, 10));
 
   const { authFetch, profile } = useContext(UserContext);
-  const sendEntry = (description, category, cost, date) => {
+  const sendEntry = (date, budget, description, category, cost) => {
     const dateArray = date.split("-");
     const body = {
-      budget: profile.budget,
+      budget: budget,
       description: description,
       category: category,
       cost: cost,
@@ -21,78 +21,90 @@ export default function AddSpend() {
       month: dateArray[1],
       year: dateArray[0],
     };
-    const options = {
-      method: "POST",
-      body: JSON.stringify(body),
-      headers: { "Content-Type": "application/json" },
-    };
+    console.log(body);
+    // const options = {
+    //   method: "POST",
+    //   body: JSON.stringify(body),
+    //   headers: { "Content-Type": "application/json" },
+    // };
 
-    authFetch("https://budgt-app.herokuapp.com/spend", options);
+    // authFetch("https://budgt-app.herokuapp.com/spend", options);
   };
-  return (
-    <div>
-      <h2>Add Expenditure</h2>
-      <form id="spend">
-        <input
-          required
-          type="text"
-          name="description"
-          placeholder="Short Description"
-          maxLength="255"
-          value={description}
-          onChange={(event) => {
-            setDescription(event.target.value);
-          }}
-        ></input>
-        <br />
-        <br />
-        <select
-          required
-          form="spend"
-          name="category"
-          onChange={(event) => {
-            setCategory(event.target.value);
+  if (profile) {
+    return (
+      <div>
+        <h2>Add Expenditure</h2>
+        <form
+          id="spend"
+          onSubmit={(event) => {
+            event.preventDefault();
+            sendEntry(date, profile.budget, description, category, cost);
           }}
         >
-          <option value={""}>Select a Category</option>
-          <option value={"Food"}>Food</option>
-          <option value={"Groceries"}>Groceries</option>
-          <option value={"Bills"}>Bills</option>
-          <option value={"Travel"}>Travel</option>
-          <option value={"Subscriptions"}>Subscriptions</option>
-          <option value={"Misc"}>Miscellaneous</option>
-        </select>
-        <br />
-        <br />
-        <input
-          required
-          type="number"
-          name="Cost"
-          placeholder="Cost"
-          min="0.01"
-          step="0.01"
-          value={cost}
-          onChange={(event) => {
-            setCost(event.target.value);
-          }}
-        ></input>
-        <br />
-        <br />
-        <input
-          required
-          type="date"
-          name="Date"
-          min={`${today.getFullYear()}-${today.getMonth() + 1}-01`}
-          max={today.toISOString().slice(0, 10)}
-          value={date}
-          onChange={(event) => {
-            setDate(event.target.value);
-          }}
-        ></input>
-        <br />
-        <br />
-      </form>
-      <Link to="/">Back to Dashboard</Link>
-    </div>
-  );
+          <input
+            required
+            type="text"
+            name="description"
+            placeholder="Short Description"
+            maxLength="255"
+            value={description}
+            onChange={(event) => {
+              setDescription(event.target.value);
+            }}
+          ></input>
+          <br />
+          <br />
+          <select
+            required
+            form="spend"
+            name="category"
+            onChange={(event) => {
+              setCategory(event.target.value);
+            }}
+          >
+            <option value={""}>Select a Category</option>
+            <option value={"Food"}>Food</option>
+            <option value={"Groceries"}>Groceries</option>
+            <option value={"Bills"}>Bills</option>
+            <option value={"Travel"}>Travel</option>
+            <option value={"Subscriptions"}>Subscriptions</option>
+            <option value={"Misc"}>Miscellaneous</option>
+          </select>
+          <br />
+          <br />
+          <input
+            required
+            type="number"
+            name="Cost"
+            placeholder="Cost"
+            min="0.01"
+            step="0.01"
+            value={cost}
+            onChange={(event) => {
+              setCost(event.target.value);
+            }}
+          ></input>
+          <br />
+          <br />
+          <input
+            required
+            type="date"
+            name="Date"
+            min={`${today.getFullYear()}-${today.getMonth() + 1}-01`}
+            max={today.toISOString().slice(0, 10)}
+            value={date}
+            onChange={(event) => {
+              setDate(event.target.value);
+            }}
+          ></input>
+          <br />
+          <br />
+          <input type="submit" value="Submit" />
+        </form>
+        <Link to="/">Back to Dashboard</Link>
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
