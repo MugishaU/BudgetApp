1b4f9e2e42d0bfa351d37623a1b07b1017cf1032
MugishaU <muwiragiye123@gmail.com>
2020-10-25 18:01:45 +0000

    import required dependencies into PieChart component

--- client/src/Components/Auth/PieChart.js
+++ client/src/Components/Auth/PieChart.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Doughnut } from "react-chartjs-2";
 
 export default function PieChart(props) {
   return <div></div>;
