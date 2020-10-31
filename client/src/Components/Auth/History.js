a6d207066719f7b32b94431451fa0040ba9fc874
MugishaU <muwiragiye123@gmail.com>
2020-10-31 22:45:18 +0000

    pass date into sendDate function from form

--- client/src/Components/Auth/History.js
+++ client/src/Components/Auth/History.js
@@ -10,14 +10,25 @@ export default function History() {
     month = String(month);
     month = "0" + month;
   }
+  const { authFetch } = useContext(UserContext);
   const [date, setDate] = useState("");
-  const sendDate = (date) => {};
+  const sendDate = (date) => {
+    const dateArray = date.split("-");
+    console.log(dateArray[1]);
+    console.log(dateArray[0]);
+  };
   return (
     <div>
       <h2>View History</h2>
       <h3>Select Month</h3>
-      <form>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          sendDate(date);
+        }}
+      >
         <input
+          required
           type="month"
           max={`${today.getFullYear()}-${month}`}
           value={date}
@@ -25,6 +36,9 @@ export default function History() {
             setDate(event.target.value);
           }}
         ></input>
+        <br />
+        <br />
+        <input type="submit" value="Submit" />
       </form>
     </div>
   );
