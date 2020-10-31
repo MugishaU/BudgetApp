44fbc4e9a3080fa52fd191736707d924cf58ea5f
MugishaU <muwiragiye123@gmail.com>
2020-10-31 22:39:28 +0000

    add history to hook on change

--- client/src/Components/Auth/History.js
+++ client/src/Components/Auth/History.js
@@ -10,14 +10,21 @@ export default function History() {
     month = String(month);
     month = "0" + month;
   }
-  const [date, setDate] = useState(null);
+  const [date, setDate] = useState("");
   const sendDate = (date) => {};
   return (
     <div>
       <h2>View History</h2>
       <h3>Select Month</h3>
       <form>
-        <input type="month" max={`${today.getFullYear()}-${month}`}></input>
+        <input
+          type="month"
+          max={`${today.getFullYear()}-${month}`}
+          value={date}
+          onChange={(event) => {
+            setDate(event.target.value);
+          }}
+        ></input>
       </form>
     </div>
   );
