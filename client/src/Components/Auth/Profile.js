b1a668ebfcad8ee12024d49dd8b0b85eae7d2e95
MugishaU <muwiragiye123@gmail.com>
2020-11-01 23:55:48 +0000

    Add fetch to update budget in db

--- client/src/Components/Auth/Profile.js
+++ client/src/Components/Auth/Profile.js
@@ -1,10 +1,34 @@
 import React, { useContext, useState, useEffect } from "react";
 import { UserContext } from "../../Context/userContext ";
+import { Router, withRouter } from "react-router";
 import * as firebase from "firebase";
 
-export default function Profile() {
-  const { profile } = useContext(UserContext);
+export default withRouter(function Profile(props) {
+  const { profile, authFetch } = useContext(UserContext);
   const [budget, setBudget] = useState("");
+  const sendBudget = async (budget) => {
+    const body = {
+      budget: budget,
+    };
+    const options = {
+      method: "PUT",
+      body: JSON.stringify(body),
+      headers: { "Content-Type": "application/json" },
+    };
+    const fetch = await authFetch(
+      "https://budgt-app.herokuapp.com/budget",
+      options
+    );
+    const fetchResult = await fetch.json();
+    const fetchError = fetchResult.error;
+
+    if (!fetchError) {
+      alert(fetchResult);
+      props.history.push("/");
+    } else {
+      alert(fetchError);
+    }
+  };
   useEffect(() => {
     setBudget(profile.budget);
   }, [profile]);
@@ -17,7 +41,7 @@ export default function Profile() {
         onSubmit={(event) => {
           event.preventDefault();
           if (budget != profile.budget) {
-            console.log(budget);
+            sendBudget(budget);
           } else {
             alert("Budget Unchanged");
           }
@@ -42,4 +66,4 @@ export default function Profile() {
       </form>
     </div>
   );
-}
+});
