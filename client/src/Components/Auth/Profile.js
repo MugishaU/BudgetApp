298b6d182a8eea7c5dd948b99c2f4346b9f513ef
MugishaU <muwiragiye123@gmail.com>
2020-11-01 23:45:25 +0000

    create change budget form pre-filled with current budget

--- client/src/Components/Auth/Profile.js
+++ client/src/Components/Auth/Profile.js
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
+import { UserContext } from "../../Context/userContext ";
+import * as firebase from "firebase";
 
 export default function Profile() {
+  const { profile } = useContext(UserContext);
+  const [budget, setBudget] = useState("");
+  useEffect(() => {
+    setBudget(profile.budget);
+  }, [profile]);
   return (
     <div>
       <h2>Profile</h2>
+      <h3>Username: {profile.username}</h3>
+      <h3>Email: {firebase.auth().currentUser.email}</h3>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          if (budget != profile.budget) {
+            console.log(budget);
+          } else {
+            alert("Budget Unchanged");
+          }
+        }}
+      >
+        <label>
+          Budget:{" "}
+          <input
+            required
+            type="number"
+            name="Budget"
+            placeholder="Budget"
+            min="0.01"
+            step="0.01"
+            value={budget}
+            onChange={(event) => {
+              setBudget(event.target.value);
+            }}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
