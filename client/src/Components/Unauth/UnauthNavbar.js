a998611ae9e3b492ac01c0b99c1c986a7928a5ec
MugishaU <muwiragiye123@gmail.com>
2020-11-03 16:35:48 +0000

    add bootstrap navbar

--- client/src/Components/Unauth/UnauthNavbar.js
+++ client/src/Components/Unauth/UnauthNavbar.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
 
 export default function UnauthNavbar() {
   return (
     <nav>
-      <NavLink to="/">Login</NavLink>
-      <NavLink to="/register">Register</NavLink>
+      <Navbar bg="light">
+        <Navbar.Text>BDGT</Navbar.Text>
+        <NavLink className="nav-link" to="/">
+          Login
+        </NavLink>
+        <NavLink className="nav-link" to="/register">
+          Register
+        </NavLink>
+      </Navbar>
     </nav>
   );
 }
