89c7b99ce5a5f5c9e36a835c5c051e4e4067df7a
MugishaU <muwiragiye123@gmail.com>
2020-11-04 21:35:01 +0000

    inline style margin auto on error404

--- client/src/Components/Error404.js
+++ client/src/Components/Error404.js
@@ -3,7 +3,7 @@ import { NavLink, withRouter } from "react-router-dom";
 
 export default withRouter(function Error404(props) {
   return (
-    <div>
+    <div style={{ margin: "auto" }}>
       <h2>Oops!</h2>
       <h4>We don't have a page called "{props.location.pathname.slice(1)}"</h4>
       <NavLink to="/">Rescue Me</NavLink>
