1a7832bbd53da355413bc696708a276e15cf1385
MugishaU <muwiragiye123@gmail.com>
2020-11-04 21:38:08 +0000

    add grid to dashboard

--- client/src/Components/Auth/Dashboard.js
+++ client/src/Components/Auth/Dashboard.js
@@ -2,6 +2,10 @@ import React, { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { UserContext } from "../../Context/userContext ";
 import { ProfileCard, LineChart, PieChart } from "../index/index";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 export default function Dashboard() {
   const { dashboard, setDashboard, profile, history, breakdown } = useContext(
@@ -14,10 +18,27 @@ export default function Dashboard() {
 
   return (
     <div>
-      <Link to="/spend">Add Expenditure</Link>
-      <ProfileCard profile={profile} history={history} />
-      <LineChart history={history} />
-      <PieChart breakdown={breakdown} />
+      <Container>
+        <Row>
+          <Col>
+            <Button to="/spend" as={Link}>
+              Add Expenditure
+            </Button>
+          </Col>
+          <Col>
+            <ProfileCard profile={profile} history={history} />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <LineChart history={history} />
+          </Col>
+          <Col>
+            <PieChart breakdown={breakdown} />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
