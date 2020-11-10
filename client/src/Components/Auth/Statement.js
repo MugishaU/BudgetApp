84ccf98ceb4d6b093e460901eb4cf57f44b2c815
MugishaU <muwiragiye123@gmail.com>
2020-11-10 18:39:20 +0000

    make statement table with date shown with st,nd,rd,th

--- client/src/Components/Auth/Statement.js
+++ client/src/Components/Auth/Statement.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useContext } from "react";
 import { UserContext } from "../../Context/userContext ";
+import Table from "react-bootstrap/Table";
 
 export default function Statement(props) {
   const { history } = useContext(UserContext);
@@ -47,17 +48,38 @@ export default function Statement(props) {
     "December",
   ];
 
+  const dateEnd = ["th", "st", "nd", "rd"];
+
   return (
     <div>
       <h2 className="graduate">
         {month[date.getMonth()]} {date.getFullYear()} Statement
       </h2>
-      <ul>
-        {sortedHistory &&
-          sortedHistory.map((item, idx) => (
-            <li key={idx}>{item.description}</li>
-          ))}
-      </ul>
+      <Table striped bordered>
+        <tbody>
+          <tr>
+            <th className="text-center graduate">Description</th>
+            <th className="text-center graduate">Category</th>
+            <th className="text-center graduate">Day</th>
+            <th className="text-center graduate">Cost</th>
+          </tr>
+          {sortedHistory &&
+            sortedHistory.map((item, idx) => (
+              <tr key={idx}>
+                <td>{item.description}</td>
+                <td>{item.category}</td>
+                <td className="text-right">
+                  {String(item.day).split("").slice(-1) > 0 &&
+                  String(item.day).split("").slice(-1) <= 3
+                    ? item.day +
+                      dateEnd[Number(String(item.day).split("").slice(-1))]
+                    : item.day + dateEnd[0]}
+                </td>
+                <td className="text-right">£{item.cost.toFixed(2)}</td>
+              </tr>
+            ))}
+        </tbody>
+      </Table>
     </div>
   );
 }
